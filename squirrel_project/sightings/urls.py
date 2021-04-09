17e2b4f4ed50124fc406cab28a296e96ac4d1a1c
davidmargatan <80817730+davidmargatan@users.noreply.github.com>
2021-04-09 14:02:32 +0700

    add urls

--- squirrel_project/sightings/urls.py
+++ squirrel_project/sightings/urls.py
@@ -1,7 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
 from django.urls import path
 
 from . import views
 
-urlpatterns = [
-    path('', views.index)
+directory = 'sightings'
+url_list = [
+    path('', views.sightings_view, name = 'sightings'),
+    path('add/', views.add_view, name = 'add'),
+    path('stats/', views.stats_view, name = 'stats'),
+    path('<str:unique_squirrel_id>/', views.update_view, name = 'update'),
 ]
+
