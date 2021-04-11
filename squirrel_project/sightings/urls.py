a7a4ea55fa51aeed9252507a6af1e2679742ccad
James Van Hess <41968228+James-VanHess@users.noreply.github.com>
2021-04-10 23:19:09 -0400

    updated urls.py, views.py, settings.py and Add_HTML.html

--- squirrel_project/sightings/urls.py
+++ squirrel_project/sightings/urls.py
@@ -5,14 +5,15 @@
 from django.urls import path
 from . import views
 from django.conf.urls import url
-from .views import sightings_view, add_view, update_view, map_view, stats_view
 
-app_name = 'sightings'
+#app_name = 'sightings'
+
 urlpatterns = [
-    path('', views.sightings_view, name = 'sightings'),
-    path('add/', views.add_view, name = 'add'),
-    path('stats/', views.stats_view, name = 'stats'),
-    path('<str:unique_squirrel_id>/', views.update_view, name = 'update'),
+    path('', views.sightings_view, name ='sightings'),
+    path('sightings/', views.sightings_view, name='sightings'),
+    path('sightings/add/', views.add_view, name = 'add'),
+    path('sightings/stats/', views.stats_view, name = 'stats'),
+    path('sightings/update/<str:squirrel_id>/', views.update_view, name = 'update'),
     path('map/', views.map_view, name = 'map'),
 ]
 
--- squirrel_project/sightings/views.py
+++ squirrel_project/sightings/views.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 
-
+import json
 import django
 from django.db import DatabaseError
 from django.db.models import Count, Avg, Max, Min
@@ -10,12 +10,13 @@ from django.forms.models import model_to_dict
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404, render, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.decorators.http import require_POST
-from django.views.generic import ListView
+from django.views.generic import TemplateView, ListView
 from django.views.generic.base import View
-from django.views.generic.edit import CreateView, DeleteView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic.list import ListView
 from django.views.decorators.clickjacking import xframe_options_sameorigin
 from django.core.exceptions import ImproperlyConfigured
 from django.contrib.auth import REDIRECT_FIELD_NAME
@@ -28,12 +29,8 @@ from .forms import django_form
 
 def sightings_view(request):
     view_data = Squirrel.objects.all()
-    paginator = Paginator(view_data, 36)
-
-    page = request.GET.get('page')
-    view_data = paginator.get_page(page)
     context = {'squirrel':view_data}
-   
+
     return render(request, 'sightings/Sightings_HTML.html', context)
 
 
@@ -49,14 +46,27 @@ def add_view(request):
 
 
 def update_view(request, unique_squirrel_id):
-    update_sighting = get_object_or_404(Squirrel, unique_squirrel_id = unique_squirrel_id)
-    if request.method == "POST":
-        form = django_form(request.POST, instance = update_sighting)
-        if form.is_valid():
-            form.save()
-            return redirect("/sightings/")
-    form = django_form(instance=update_sighting)
-    return render(request, "sightings/Update_HTML.html", {"form": form, "unique_squirrel_id": unique_squirrel_id})
+    Object = get_object_or_404(Squirrel,Unique_squirrel_id=squirrel_id)
+    form = django_form(request.POST or None,instance=Object)
+    context = {'form':form}
+    if form.is_valid():
+        Object=form.save(commit=False)
+        Object.save()
+        return redirect('/sightings/')
+    else:
+        context={
+                'form':form,
+        }
+        return render(request,'sightings/Update_HTML.html',context)
+    
+    # update_sighting = get_object_or_404(Squirrel, unique_squirrel_id = unique_squirrel_id)
+    # if request.method == "POST":
+    #     form = django_form(request.POST, instance = update_sighting)
+    #     if form.is_valid():
+    #         form.save()
+    #         return redirect("/sightings/")
+    # form = django_form(instance=update_sighting)
+    # return render(request, "sightings/Update_HTML.html", {"form": form, "unique_squirrel_id": unique_squirrel_id})
 
 
 def stats_view(request):
